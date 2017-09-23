dbb676dae0511cbbd7a116badc45c5279ff2bf8e
MrKaku <kebiguohong@sina.com>
2017-09-23 14:41:52 +0000

    Finish user singup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,23 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        //create user in datebase
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', 'Welcome! You will have a new travel');
+        return redirect()->route('users.show', [$user]);
+    }
 }
